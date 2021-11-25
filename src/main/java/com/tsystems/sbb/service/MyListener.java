99711a9565953f5c479533d0939af6ef38a74aab
vgrigory <grigoryevvadim@mail.ru>
2021-11-26 02:23:47 +0300

    new listener is added

--- src/main/java/com/tsystems/sbb/service/MyListener.java
+++ src/main/java/com/tsystems/sbb/service/MyListener.java
@@ -1,9 +1,11 @@
 package com.tsystems.sbb.service;
 
 import com.tsystems.sbb.entity.BoardDTO;
+import com.tsystems.sbb.entity.Greeting;
 import com.tsystems.sbb.entity.ScheduleDTO;
 import com.tsystems.sbb.entity.User;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -15,9 +17,14 @@ public class MyListener {
 //        System.out.println("Message from the queue: " + string);
 //    }
 
-
     @RabbitListener(queues = "schedules")
     public void listenUser(List<BoardDTO> boards) {
         MyListener.boards = boards;
+    }
+
+    @RabbitListener(queues = "notification")
+    @SendTo("/topic/greetings")
+    public Greeting listenNotification(String notification) {
+        return new Greeting();
     }
 }
